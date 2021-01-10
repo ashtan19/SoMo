36c91123eeb823a98d71e515c90fad330f4e110a
Kevin Liang <kevinjiahongliang@gmail.com>
2021-01-09 16:41:30 -0800

    working version

--- src/server/twitter_index.js
+++ src/server/twitter_index.js
@@ -1,13 +1,13 @@
 const { response } = require("express");
 const express = require("express");
-const axios = require("axios")
-const keys = require("./keys")
+const axios = require("axios");
+const keys = require("./keys");
 
 const Router = express.Router();
 const twitterURL = axios.create({
   baseURL: "https://api.twitter.com",
-  headers: {'Authorization': `Bearer ${keys.bearer}`}
-})
+  headers: { Authorization: `Bearer ${keys.bearer}` },
+});
 
 // twitterURL.defaults.headers.common = {'Authorization': `Bearer ${keys.bearer}`}
 
@@ -17,7 +17,6 @@ const twitterURL = axios.create({
 // const bodyParameters = {
 //   key: "value"
 // };
-
 
 Router.get("/top", async (req, res) => {
   try {
@@ -29,16 +28,59 @@ Router.get("/top", async (req, res) => {
 });
 
 Router.get("/search", async (req, res) => {
-  try {
+  let queryString = "%23" + req.query.queryString;
+  console.log("queryString: ", queryString);
+  let filteredTweets = [];
+  let loopCount = 0;
+  let nextTokenString = "";
 
-    twitterURL.get("/2/tweets/search/recent?query=blm").then((val) => {
-      console.log("PRINTING TWITTER RESPONSE");
-      console.log(val.data);
-      res.status(200).send(val.data)
-    }).catch(e => {
-      console.log(e)
-      res.status(404).send("error!!")
-    })
+  try {
+    console.log("PRINTING TWITTER RESPONSE");
+    while (loopCount < 3) {
+      loopCount++;
+      let nextToken = "";
+      if (loopCount > 1) {
+        nextToken = "&next_token=" + nextTokenString;
+      }
+      await twitterURL
+        .get(
+          "/2/tweets/search/recent?max_results=100&tweet.fields=public_metrics&query=" +
+            queryString +
+            nextToken
+        )
+        .then((val) => {
+          console.log("iteration");
+          console.log(val.data);
+          nextTokenString = val.data.meta.next_token;
+
+          console.log("nextTokenString, ", nextTokenString);
+
+          let tweets = val.data.data;
+          for (let tweet of tweets) {
+            console.log("likeCount", tweet.public_metrics.like_count);
+            if (
+              tweet.public_metrics.retweet_count +
+                tweet.public_metrics.like_count +
+                tweet.public_metrics.quote_count +
+                tweet.public_metrics.reply_count >=
+              1000
+            ) {
+              filteredTweets.push(tweet);
+            }
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+    console.log(
+      "number of tweets computed:",
+      filteredTweets.length,
+      "using ",
+      loopCount,
+      " loops"
+    );
+    res.status(200).send(filteredTweets);
   } catch (error) {
     console.log(error);
   }
