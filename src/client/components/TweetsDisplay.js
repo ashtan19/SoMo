fab35f6fa5864a5525617b22de46dd6c68808b38
Kevin Liang <kevinjiahongliang@gmail.com>
2021-01-09 22:28:56 -0800

    working twitter scroll feed

--- src/client/components/TweetsDisplay.js
+++ src/client/components/TweetsDisplay.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "../CSS/TweetsDisplayStyles.css";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export const TweetsDisplay = ({ tweets }) => {
-  return <div>tweets go here</div>;
+  const [feed, setFeed] = useState(tweets);
+  React.useEffect(() => {
+    console.log("rendering tweets display");
+    console.log("tweets", tweets[0]);
+    setFeed(tweets);
+  }, [tweets]);
+
+  const presentTweets = () => {
+    console.log("feed: ", feed);
+    return feed.slice(0, 10).map((tweet) => {
+      console.log(tweet.id);
+      return (
+        <TwitterTweetEmbed
+          key={tweet.id}
+          tweetId={tweet.id.toString()}
+        ></TwitterTweetEmbed>
+      );
+    });
+  };
+  return <div className="tweets-container">{presentTweets()}</div>;
 };
